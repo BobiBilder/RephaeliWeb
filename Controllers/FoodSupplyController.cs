d2a033cdbb05b90ef8182d6ddeb348b96ba2bd3d
tomer bilder <bilder52@outlook.com>
2022-02-27 08:42:15 +0200

    WebService

---
+++ Controllers/FoodSupplyController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MasterProject.FoodSupplier;
+
+namespace MasterProject.Controllers
+{
+    public class FoodSupplyController : Controller
+    {
+        // GET: FoodSupply
+        public ActionResult FoodSupplyForm()
+        {
+            return View(0.0);
+        }
+
+
+        public ActionResult PutSupply(int num1, int num2, int action)
+        {
+            ServiceClient service = new ServiceClient();
+            double sum = 0;
+            switch (action)
+            {
+                case 1:
+                    sum = service.Plus(num1, num2);
+                    break;
+                case 2:
+                    sum = service.Minus(num1, num2);
+                    break;
+                case 3:
+                    sum = service.Multi(num1, num2);
+                    break;
+                case 4:
+                    sum = service.Divide(num1, num2);
+                    break;
+            }
+            return View("FoodSupplyForm",sum);
+        }
+    }
+}
